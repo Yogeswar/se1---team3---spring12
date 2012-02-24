8db3dfbc1d129d6919bef020051f4b37663c35cc
fenzo333 <fenzo333@gmail.com>
2012-02-24 23:47:52 +0000

    Base html file

---
+++ SalesAssist/src/com/SET3/SalesAssist/SalesAssistActivity.java
@@ -0,0 +1,13 @@
+package com.SET3.SalesAssist;
+
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class SalesAssistActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
